0b54b33ecac27495ff4342b28b31815a1629bc90
Rafael Honorato <rafaelhonnorato@gmail.com>
2026-04-24 22:30:14 -0300

    refac: adição de pipes  e eestilos

--- front/src/app/features/dashboard/dashboard.component.ts
+++ front/src/app/features/dashboard/dashboard.component.ts
@@ -1,34 +1,43 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { LayoutService } from '../../core/services/layout.service';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { StockCardComponent } from './cards/stock-card/stock-card.component';
 import { DashboardService } from './services/dashboard.service';
-import { Observable } from 'rxjs';
-import { SalesCardComponent } from './cards/sales-card/sales-card.component';
+import { BasicCardComponent } from './cards/basic-card/basic-card.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ButtonModule, CommonModule, StockCardComponent, SalesCardComponent],
+  imports: [ButtonModule, CommonModule, StockCardComponent, BasicCardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements OnInit {
   layoutService = inject(LayoutService);
   dashboardService = inject(DashboardService);
-  stock$!: Observable<{
-    totalCostPrice: number;
-    totalSalesPrice: number;
-    quantity: number;
-  }>;
-  totalSales$!: Observable<{ value: number; quantity: number }>;
+
+  metrics = toSignal(this.dashboardService.getDashboardMetrics(), {
+    initialValue: {
+      stock: { totalCostPrice: 0, totalSalesPrice: 0, quantity: 0 },
+      sales: { value: 0, quantity: 0 },
+      estimatedProfit: 0,
+      coveragePercentage: 0,
+      statusColor: 'text-slate-50',
+    },
+  });
+
+  averageTicked = toSignal(this.dashboardService.getAverageTicket(), {
+    initialValue: {
+      average: 0,
+      quantity: 0,
+    },
+  });
+
   ngOnInit() {
     this.layoutService.setHeader(
       'Dashboard',
       'Aqui está o resumo do seu estoque',
     );
-
-    this.stock$ = this.dashboardService.getTotalValueStock();
-    this.totalSales$ = this.dashboardService.getTotalSales();
   }
 }
--- front/src/app/features/dashboard/services/dashboard.service.ts
+++ front/src/app/features/dashboard/services/dashboard.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
-import { map, Observable } from 'rxjs';
+import { forkJoin, map, Observable } from 'rxjs';
 import { Stock } from '../../../core/interfaces/stock';
 import { Sales } from '../../../core/interfaces/sales';
+import { DashboardMetrics } from '../../../core/interfaces/dashboardMetrics';
 
 @Injectable({
   providedIn: 'root',
@@ -49,5 +50,55 @@ export class DashboardService {
         };
       }),
     );
+  }
+
+  getDashboardMetrics(): Observable<DashboardMetrics> {
+    return forkJoin({
+      stock: this.getTotalValueStock(),
+      sales: this.getTotalSales(),
+    }).pipe(
+      map(({ stock, sales }) => {
+        const estimatedProfit = stock.totalSalesPrice - stock.totalCostPrice;
+        const coveragePercentage =
+          stock.totalCostPrice > 0
+            ? (sales.value / stock.totalCostPrice) * 100
+            : 0;
+
+        return {
+          stock,
+          sales,
+          estimatedProfit,
+          coveragePercentage,
+          statusColor: this.calculateStatusColor(coveragePercentage),
+        };
+      }),
+    );
+  }
+
+  getAverageTicket(): Observable<{ average: number; quantity: number }> {
+    return this.getSales().pipe(
+      map((sales) => {
+        if (sales.length === 0) return { average: 0, quantity: 0 };
+
+        const totalValue = sales.reduce(
+          (acc, sale) => acc + sale.totalPrice,
+          0,
+        );
+        const totalTransactions = sales.length;
+
+        return {
+          average: Number((totalValue / totalTransactions).toFixed(2)),
+          quantity: totalTransactions,
+        };
+      }),
+    );
+  }
+
+  private calculateStatusColor(percentage: number): string {
+    if (percentage < 25) return 'text-red-500';
+    if (percentage < 75) return 'text-orange-500';
+    if (percentage < 100) return 'text-yellow-400';
+    if (percentage < 150) return 'text-emerald-400';
+    return 'text-cyan-400';
   }
 }
